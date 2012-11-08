2159eee15a3e177e12603d966a0f056ef0f24812
raboiten <bad_boit@msn.com>
2012-11-08 13:00:03 +0100

    lagt til topiccon

--- Minerva/src/tables/Topic.java
+++ Minerva/src/tables/Topic.java
@@ -1,14 +1,19 @@
 package tables;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 public class Topic {
 	
 	@Id
+	@GenericGenerator(name = "generator", strategy = "increment") 
+    @GeneratedValue(generator = "generator")
 	private long id;
 	private String name;
 	private String description;
@@ -26,9 +31,6 @@ public class Topic {
 		return id;
 	}
 
-	private void setId(long id) {
-		this.id = id;
-	}
 
 	public String getName() {
 		return name;
