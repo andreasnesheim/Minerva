9b63e781f3e0ad7d85d78f38e079ff722f672aaa
Joacim André Jakobsen <joacimjakobsen@gmail.com>
2012-11-11 17:29:56 +0100

    commite noen endringer som tydligvis isje va commita fra sist gang

---
+++ Minerva/src/servlet/MailServlet.java
@@ -0,0 +1,50 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import mail.GoogleMail;
+
+/**
+ * Servlet implementation class MailServlet
+ */
+@WebServlet("/MailServlet")
+public class MailServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private String signature, to, from, subject, message, password;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public MailServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		from = (String) request.getSession().getAttribute("email");
+		from ="minervatester";
+//		from.replaceAll("@gmail.com", "");
+		signature = "This is a mail sent from us by the user " + from;
+//		to = ;
+		password = "minervatester1234";
+		subject = request.getParameter("subject");
+		message = request.getParameter("text");
+		GoogleMail.Send(from, password, to, subject, message);
+	}
+
+}
