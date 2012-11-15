a3cb8e2ec07448d7121f34e7cd766c04378374ba
raboiten <bad_boit@msn.com>
2012-11-15 13:51:54 +0100

    komentar paa categoryCon

--- Minerva/src/Connection/CategoryCon.java
+++ Minerva/src/Connection/CategoryCon.java
@@ -12,6 +12,7 @@ import org.hibernate.Session;
 
 public class CategoryCon {
 
+	// Lager et MainCategory objekt og lagrer det i databasen, med navnet på kategorien som innparameter
 	public static void createMainCategory(String name) {
 
 		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
@@ -25,7 +26,7 @@ public class CategoryCon {
 		session.getTransaction().commit();
 
 	}
-
+	// Lager et SubCategory objekt og lagrer det i databasen, med navnet på kategorien som inparameter og hvilke MainCategory den ligger under
 	public static void createSubCategory(String name, long mainCategory) {
 
 		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
@@ -39,7 +40,7 @@ public class CategoryCon {
 		session.getTransaction().commit();
 
 	}
-	
+	// Henter ut SubCategory'en med id
 	public static SubCategory getSubCategory(long id) {
 
 
@@ -53,7 +54,7 @@ public class CategoryCon {
         return subCategory;
 
     }
-	
+	// Henter ut MainCategory'en med id
 	public static MainCategory getMainCategory(long id) {
 
 
@@ -67,7 +68,7 @@ public class CategoryCon {
         return mainCategory;
 
     }
-
+	// Henter ut alle MainCategory'ene
 	public static List<MainCategory> getMainCategories() {
 
 
@@ -81,7 +82,7 @@ public class CategoryCon {
 		return result;
 
 	}
-
+	// Henter ut alle SubCategory'ene
 	public static List<SubCategory> getSubCategories() {
 		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 		session.beginTransaction();
